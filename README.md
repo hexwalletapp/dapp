fdbaf256f64f85ed4efaa98ad4166e539e2686cd
Joe Blau <im@joeblau.com>
2022-07-02 15:51:25 -0700

    read stake count

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HEX Wallet Dapp
+
+Test acocunts
+
+```
+0xac28238ae676cb00d55bec84b70786a75245ff1f
+0xa4aad7a81576c632beb664fd5ecfdda4a36c519a
+```
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,28 @@
 import type { NextPage } from "next";
 import { useContractRead } from "wagmi";
 import HEXABI from "../lib/HEXABI.json";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  const {
-    data: totalSupply,
-    isError,
-    isLoading,
-  } = useContractRead({
+  const [stakeAddress, setStakeAddress] = useState("");
+  const { data: stakeCount } = useContractRead({
     addressOrName: "0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39",
     contractInterface: HEXABI,
-    functionName: "totalSupply",
+    functionName: "stakeCount",
+    args: stakeAddress,
   });
 
-  return <div>{totalSupply?.toString()}</div>;
+  return (
+    <div>
+      <h3>{stakeAddress}</h3>
+      <h4>{stakeCount?.toString()}</h4>
+      <input
+        type="text"
+        value={stakeAddress}
+        onChange={(e) => setStakeAddress(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Home;
