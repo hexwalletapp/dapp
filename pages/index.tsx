31d64c500b614607deaa8877df516bdfae10ee37
Joe Blau <im@joeblau.com>
2022-07-02 15:36:08 -0700

    add hex abi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,19 @@
 import type { NextPage } from "next";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
-import { InjectedConnector } from "wagmi/connectors/injected";
+import { useContractRead } from "wagmi";
+import HEXABI from "../lib/HEXABI.json";
 
 const Home: NextPage = () => {
-  const { address, isConnected } = useAccount();
-  const { connect } = useConnect({
-    connector: new InjectedConnector(),
+  const {
+    data: totalSupply,
+    isError,
+    isLoading,
+  } = useContractRead({
+    addressOrName: "0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39",
+    contractInterface: HEXABI,
+    functionName: "totalSupply",
   });
-  const { disconnect } = useDisconnect();
 
-  if (isConnected)
-    return (
-      <div>
-        Connected to {address}
-        <button onClick={() => disconnect()}>Disconnect</button>
-      </div>
-    );
-  return <button onClick={() => connect()}>Connect Wallet</button>;
+  return <div>{totalSupply?.toString()}</div>;
 };
 
 export default Home;
