f399f64d8ef70a54cdd4e3a2b772bfb6983de0d0
Joe Blau <im@joeblau.com>
2022-07-02 16:32:25 -0700

    show stakes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { WagmiConfig, createClient } from "wagmi";
-import { getDefaultProvider } from "ethers";
+import { WagmiConfig, createClient, chain, configureChains } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+
+const alchemyId = process.env.ALCHEMY_ID;
+
+const { provider } = configureChains(
+  [chain.mainnet],
+  [alchemyProvider({ alchemyId })]
+);
 
 const client = createClient({
   autoConnect: true,
-  provider: getDefaultProvider(),
+  provider: provider,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useContractRead } from "wagmi";
+import { useContractRead, useContractReads } from "wagmi";
 import HEXABI from "../lib/HEXABI.json";
 import { useState } from "react";
 
@@ -12,6 +12,23 @@ const Home: NextPage = () => {
     args: stakeAddress,
   });
 
+  const hexContract = {
+    addressOrName: "0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39",
+    contractInterface: HEXABI,
+  };
+
+  const allStakes = Array.from(
+    { length: Number(stakeCount) },
+    (v, stakeIndex) => ({
+      ...hexContract,
+      functionName: "stakeLists",
+      args: [stakeAddress, stakeIndex],
+    })
+  );
+  const { data, isError, isLoading } = useContractReads({
+    contracts: allStakes,
+  });
+
   return (
     <div>
       <h3>{stakeAddress}</h3>
@@ -21,6 +38,15 @@ const Home: NextPage = () => {
         value={stakeAddress}
         onChange={(e) => setStakeAddress(e.target.value)}
       />
+
+      <h4>Stakes</h4>
+      <ul>
+        {data?.map((stake, index) => (
+          <li key={index}>
+            <div>{`${stake}`}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
