be017f8509e99a00cf49a4a759a84dfa8a47f6e3
Joe Blau <im@joeblau.com>
2022-07-02 20:31:12 -0700

    added pulsechain network

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "~/styles/globals.css";
 import type { AppProps } from "next/app";
 import { WagmiConfig, createClient, chain, configureChains } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { pulseChain } from "~/lib/pulsechain";
 
 const alchemyId = process.env.ALCHEMY_ID;
 
 const { provider } = configureChains(
-  [chain.mainnet],
-  [alchemyProvider({ alchemyId })]
+  [chain.mainnet, pulseChain],
+  [alchemyProvider({ alchemyId }), publicProvider()]
 );
 
 const client = createClient({
